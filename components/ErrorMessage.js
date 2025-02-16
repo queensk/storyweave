e35f3801c914303ba69f1b7868a4abe6d38ac7be
queens kisivuli <queenskisivuli@gmail.com>
2025-02-16 06:29:46 -0800

    Add ErrorMessage component for displaying alert messages

---
+++ components/ErrorMessage.js
@@ -0,0 +1,10 @@
+const ErrorMessage = ({ message }) => (
+  <div
+    className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-2"
+    role="alert"
+  >
+    <span className="block sm:inline">{message}</span>
+  </div>
+);
+
+export default ErrorMessage;
